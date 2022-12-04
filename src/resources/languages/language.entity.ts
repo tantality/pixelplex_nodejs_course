d6fb1431d641ac4619bf8c053a2eade10740b6f8
tantality <angel.tretyak@mail.ru>
2022-12-05 01:25:12 +0300

    merge: move the types to the appropriate files (task-9 branch)

--- src/resources/languages/language.entity.ts
+++ src/resources/languages/language.entity.ts
@@ -1,18 +1,22 @@
 import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, Index } from 'typeorm';
-import { ILanguage } from './language.interface';
+import { ILanguage } from './types';
 
 @Entity('languages')
 export class Language extends BaseEntity implements ILanguage {
   @PrimaryGeneratedColumn()
     id!: number;
+
   @Index()
   @Column({ type: 'varchar', length: 255 })
     name!: string;
+
   @Column({ type: 'varchar', length: 255, unique: true })
     code!: string;
+
   @Index()
   @CreateDateColumn()
     createdAt!: Date;
+
   @UpdateDateColumn()
     updatedAt!: Date;
 }
