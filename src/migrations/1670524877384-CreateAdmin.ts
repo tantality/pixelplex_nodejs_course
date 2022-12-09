273b1a08f210042d8b017b4540657b53194f774a
tantality <angel.tretyak@mail.ru>
2022-12-09 18:14:53 +0300

    feat: remove the nativeLanguageId field from the sql query

--- src/migrations/1670524877384-CreateAdmin.ts
+++ src/migrations/1670524877384-CreateAdmin.ts
@@ -4,8 +4,8 @@ import { ADMIN } from '../modules/auth/auth.constants';
 export class CreateAdmin1670524877384 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `INSERT INTO "users" ("name", "email", "normalizedEmail", "role", "password", "nativeLanguageId") 
-       VALUES ('${ADMIN.NAME}', '${ADMIN.EMAIL}', '${ADMIN.NORMALIZED_EMAIL}', '${ADMIN.ROLE}', '${ADMIN.PASSWORD}', 14)`,
+      `INSERT INTO "users" ("name", "email", "normalizedEmail", "role", "password") 
+       VALUES ('${ADMIN.NAME}', '${ADMIN.EMAIL}', '${ADMIN.NORMALIZED_EMAIL}', '${ADMIN.ROLE}', '${ADMIN.PASSWORD}')`,
     );
   }
 
