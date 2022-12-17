c87b2e00fadabe1fa043b10aa7e1b5eeff73d935
tantality <angel.tretyak@mail.ru>
2022-12-17 05:21:29 +0300

    refactor: rename variables

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -16,8 +16,8 @@ export class UsersController {
 
   static updateUser = async (req: UpdateUserRequest, res: UpdateUserResponse, next: NextFunction): Promise<void> => {
     try {
-      const user = await UsersService.update(req.userId as number, req.body);
-      res.status(200).json(new UserDTO(user));
+      const updatedUser = await UsersService.update(req.userId as number, req.body);
+      res.status(200).json(new UserDTO(updatedUser));
     } catch (err) {
       next(err);
     }
