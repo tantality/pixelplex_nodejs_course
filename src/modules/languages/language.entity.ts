48d465e8de348f9431075fc262e169c48da2d8a4
tantality <angel.tretyak@mail.ru>
2022-12-17 04:43:47 +0300

    feat: implement and use a common entity

--- src/modules/languages/language.entity.ts
+++ src/modules/languages/language.entity.ts
@@ -1,25 +1,17 @@
-import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, Index, OneToMany, Relation } from 'typeorm';
+import { Entity, Column, Index, OneToMany, Relation } from 'typeorm';
+import { CommonEntity } from '../../entities';
 import { User } from '../users/user.entity';
 import { MAX_CODE_LENGTH, MAX_NAME_LENGTH } from './languages.constants';
 import { ILanguage } from './types';
 
 @Entity('languages')
-export class Language extends BaseEntity implements ILanguage {
-  @PrimaryGeneratedColumn()
-    id!: number;
-
+@Index(['createdAt'])
+export class Language extends CommonEntity implements ILanguage {
   @Column({ type: 'varchar', length: MAX_NAME_LENGTH })
     name!: string;
 
   @Column({ type: 'varchar', length: MAX_CODE_LENGTH, unique: true })
     code!: string;
-
-  @Index()
-  @CreateDateColumn()
-    createdAt!: Date;
-
-  @UpdateDateColumn()
-    updatedAt!: Date;
 
   @OneToMany(() => User, (user) => user.nativeLanguage)
     users!: Relation<User>[];
