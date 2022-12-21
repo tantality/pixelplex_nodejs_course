91617070e9b6d6245d97e54dc0e2e432ce66ea01
tantality <angel.tretyak@mail.ru>
2022-12-21 20:28:42 +0300

    refactor: move constants

--- src/migrations/1670524877384-CreateAdmin.ts
+++ src/migrations/1670524877384-CreateAdmin.ts
@@ -1,5 +1,18 @@
+import normalizeEmail from 'normalize-email';
 import { MigrationInterface, QueryRunner } from 'typeorm';
-import { ADMIN } from '../modules/auth/auth.constants';
+import * as bcrypt from 'bcrypt';
+import * as dotenv from 'dotenv';
+import { SALT_ROUNDS } from '../modules/auth/auth.constants';
+import { USER_ROLE } from '../modules/users/types';
+dotenv.config();
+
+const ADMIN = {
+  NAME: 'admin',
+  EMAIL: process.env.ADMIN_EMAIL as string,
+  NORMALIZED_EMAIL: normalizeEmail(process.env.ADMIN_EMAIL as string),
+  PASSWORD: await bcrypt.hash(process.env.ADMIN_PASSWORD as string, SALT_ROUNDS),
+  ROLE: USER_ROLE.ADMIN,
+};
 
 export class CreateAdmin1670524877384 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
