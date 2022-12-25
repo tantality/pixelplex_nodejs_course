746aa0ea10d91979674a9172e551029446632784
tantality <angel.tretyak@mail.ru>
2022-12-26 01:51:27 +0300

    refactor: move functions

--- src/modules/cards/utils/create-subquery-to-group-words.utils.ts
+++
@@ -1,8 +0,0 @@
-export const createSubqueryToGroupWords = (findWordsSubquery: string, orderByWordCondition: string): string => {
-  return `
-    (
-      SELECT ARRAY_AGG("w"."id" ${orderByWordCondition}) AS "ids", ARRAY_AGG("w"."value" ${orderByWordCondition}) AS "values", "w"."cardId" AS "cardId"
-      FROM ${findWordsSubquery} AS "w"
-      GROUP BY "w"."cardId"
-    )`;
-};
