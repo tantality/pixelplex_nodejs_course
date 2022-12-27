c3bf11556e8ea65b6a033c59bc4f71b41927dc79
tantality <angel.tretyak@mail.ru>
2022-12-27 15:57:40 +0300

    refactor: use the ternary operator

--- src/modules/tasks/utils/get-answer-status.utils.ts
+++ src/modules/tasks/utils/get-answer-status.utils.ts
@@ -2,9 +2,5 @@ import { TASK_STATUS } from '../types';
 
 export const getAnswerStatus = (correctAnswers: string[], receivedAnswer: string): string => {
   const isReceivedAnswerCorrect = correctAnswers.includes(receivedAnswer);
-  if (isReceivedAnswerCorrect) {
-    return TASK_STATUS.CORRECT;
-  } else {
-    return TASK_STATUS.INCORRECT;
-  }
+  return isReceivedAnswerCorrect ? TASK_STATUS.CORRECT : TASK_STATUS.INCORRECT;
 };
