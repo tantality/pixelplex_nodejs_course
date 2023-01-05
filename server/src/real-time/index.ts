cbe5125e72f8c91eb81760a656d5010ab1f9fedc
tantality <angel.tretyak@mail.ru>
2023-01-05 23:00:20 +0300

    feat: implement sending notifications to authorized users

--- server/src/real-time/index.ts
+++ server/src/real-time/index.ts
@@ -1,10 +1,19 @@
 import http from 'http';
-import { Server } from 'socket.io';
-import { registerLanguageHandlers } from './language/language.handler';
+import { Server, Socket } from 'socket.io';
+import { registerLanguageHandlers } from './languages/language.handler';
+import { isAuth } from './middleware';
 import { IO_SERVER_OPTIONS } from './real-time.constants';
+import { ROOMS } from './real-time.types';
+
+const onConnection = (socket: Socket): void => {
+  socket.join(ROOMS.AUTH_USERS);
+};
 
 const initSocket = (server: http.Server): void => {
   const io = new Server(server, IO_SERVER_OPTIONS);
+  io.use(isAuth);
+  io.on('connection', onConnection);
+
   registerLanguageHandlers(io);
 };
 
--- server/src/real-time/languages/language.handler.ts
+++ server/src/real-time/languages/language.handler.ts
@@ -1,17 +1,15 @@
 import EventEmitter from 'events';
 import { Server } from 'socket.io';
-import { DefaultEventsMap } from 'socket.io/dist/typed-events';
 import TypedEmitter from 'typed-emitter';
 import { ILanguage } from '../../modules/languages/types';
+import { EVENTS, ROOMS } from '../real-time.types';
 import { LanguageEvents, LANGUAGE_EVENTS } from './language.types';
 
 export const languageEventEmitter = new EventEmitter() as TypedEmitter<LanguageEvents>;
 
-export const registerLanguageHandlers = (io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>): void => {
+export const registerLanguageHandlers = (io: Server): void => {
   const createLanguage = (payload: ILanguage): void => {
-    io.sockets.sockets.forEach((client) =>
-      client.send(JSON.stringify({ event: LANGUAGE_EVENTS.CREATE, payload: JSON.stringify(payload) })),
-    );
+    io.to(ROOMS.AUTH_USERS).emit(EVENTS.MESSAGE, JSON.stringify({ event: LANGUAGE_EVENTS.CREATE, payload: JSON.stringify(payload) }));
   };
 
   languageEventEmitter.on(LANGUAGE_EVENTS.CREATE, createLanguage);
